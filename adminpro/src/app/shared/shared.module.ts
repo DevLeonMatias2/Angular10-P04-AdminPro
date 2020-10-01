e7d5280b41180d4ddcbb85b630783e4e26379bf0
rleon <soluciondigital15@farmaciasperuanas.pe>
2020-10-01 10:10:46 -0500

    Fin de la seccion 7

--- adminpro/src/app/shared/shared.module.ts
+++ adminpro/src/app/shared/shared.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule} from "@angular/router";
+
 import {BreadcrumbsComponent} from "./breadcrumbs/breadcrumbs.component";
 import {SidebarComponent} from "./sidebar/sidebar.component";
 import {HeaderComponent} from "./header/header.component";
@@ -19,9 +21,10 @@ import {HeaderComponent} from "./header/header.component";
   ],
 
 
-  imports: [
-    CommonModule,
+    imports: [
+        CommonModule,
+        RouterModule,
 
-  ]
+    ]
 })
 export class SharedModule { }
